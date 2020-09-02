cc99e268515058980bffdad68ea3d034b6e70266
Malcolm Greaves <malcolmgreaves@users.noreply.github.com>
2020-09-01 22:17:08 -0700

    format

--- tests/test_iteration.py
+++ tests/test_iteration.py
@@ -2,19 +2,19 @@ from core_utils.iteration import yield_all
 
 
 def test_yield_all():
-  def f():
-     yield 1
-     yield 2
+    def f():
+        yield 1
+        yield 2
 
-  def z():
-    for x in [f,f,f,f]: yield x()
+    def z():
+        for x in [f,f,f,f]: yield x()
 
-  assert list(yield_all(z())) == [1, 2, 1, 2, 1, 2, 1, 2]
+    assert list(yield_all(z())) == [1, 2, 1, 2, 1, 2, 1, 2]
 
-  def a():
-    for x in [z,z,z,z]: yield x()
+    def a():
+        for x in [z,z,z,z]: yield x()
 
-  def b():
-    for x in [a,a,a,a]: yield x()
+    def b():
+        for x in [a,a,a,a]: yield x()
 
-  assert list(yield_all(b())) == [1,2] * 64
+    assert list(yield_all(b())) == [1,2] * 64
