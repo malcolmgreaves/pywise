c540d384984465850b234cdf65ef0ec4232c7192
Malcolm Greaves <malcolmgreaves@users.noreply.github.com>
2020-09-01 22:09:16 -0700

    test the yield_all function

---
+++ tests/test_iteration.py
@@ -0,0 +1,17 @@
+def test_yield_all():
+  def f():
+     yield 1
+     yield 2
+
+  def z():
+    for x in [f,f,f,f]: yield x()
+
+  assert list(yield_all(z())) == [1, 2, 1, 2, 1, 2, 1, 2]
+
+  def a():
+    for x in [z,z,z,z]: yield x()
+
+  def b():
+    for x in [a,a,a,a]: yield x()
+
+  assert list(yield_all(b())) == [1,2] * 64
