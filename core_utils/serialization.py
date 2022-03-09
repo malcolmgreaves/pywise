e2635d6fedc8ce407f8f2bbb6bd5cf593afeb11d
Malcolm Greaves <greaves.malcolm@gmail.com>
2022-03-08 19:11:35 -0800

    fixed -- cannot support ForwardRef w/o needing to evaluate properly @ runtime

--- core_utils/serialization.py
+++ core_utils/serialization.py
@@ -127,10 +127,6 @@ def deserialize(
     NOTE: If using :param:`custom` for generic types, you *must* have unique instances for each possible
           type parametrization.
     """
-
-    import ipdb
-    ipdb.set_trace()
-    
     if custom is not None and type_value in custom:
         return custom[type_value](value)
 
--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -1,6 +1,3 @@
-from __future__ import annotations
-
-
 import io
 import json
 from collections import namedtuple
@@ -523,26 +520,41 @@ def test_serialize_has_defaults_nt():
     assert serialize(deserialize(HasDefaultsNT, serialize(HasDefaultsNT()))) == s
 
 
+class Next3(NamedTuple):
+    dc_value: HasDefaultsDC = HasDefaultsDC()
+    nt_name: HasDefaultsNT = HasDefaultsNT()
+
+
+class Next2(NamedTuple):
+    dc_value: HasDefaultsDC = HasDefaultsDC()
+    nt_name: HasDefaultsNT = HasDefaultsNT()
+    next: Optional[Next3] = None
+
+
+class Next1(NamedTuple):
+    dc_value: HasDefaultsDC = HasDefaultsDC()
+    nt_name: HasDefaultsNT = HasDefaultsNT()
+    next: Optional[Next2] = None
 
 
 @dataclass(frozen=True)
 class NestedDefaultsMixed:
     dc_value: HasDefaultsDC = HasDefaultsDC()
     nt_name: HasDefaultsNT = HasDefaultsNT()
-    recursive: Optional[NestedDefaultsMixed] = None
+    next: Optional[Next1] = None
 
 
 def test_serialized_nested_defaults_basic():
     x: NestedDefaultsMixed = deserialize(NestedDefaultsMixed, {})
     assert x.dc_value.value == 10
     assert x.nt_name.name == "<noname>"
-    assert x.recursive is None
+    assert x.next is None
 
     s = serialize(x, no_none_values=False)
     assert s == {
         "dc_value": {"value": 10},
         "nt_name": {"name": "<noname>"},
-        "recursive": None,
+        "next": None,
     }
     assert (
         serialize(
@@ -557,13 +569,11 @@ def test_serialized_nested_defaults_advanced():
     nested = NestedDefaultsMixed(
         dc_value=HasDefaultsDC(9999),
         nt_name=HasDefaultsNT("powerlevel"),
-        recursive=NestedDefaultsMixed(
+        next=Next1(
             nt_name=HasDefaultsNT("hello world!"),
-            recursive=NestedDefaultsMixed(
+            next=Next2(
                 dc_value=HasDefaultsDC(-50),
-                recursive=NestedDefaultsMixed(
-                    nt_name=HasDefaultsNT("goodbye universe?"),
-                ),
+                next=Next3(nt_name=HasDefaultsNT("goodbye universe?"),),
             ),
         ),
     )
