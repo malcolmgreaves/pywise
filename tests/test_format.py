9779114ad3d7650435d95034ddddbac60c0e470e
Malcolm Greaves <greaves.malcolm@gmail.com>
2024-05-29 11:59:38 +0900

    test program_init_param_msg

--- tests/test_format.py
+++ tests/test_format.py
@@ -18,7 +18,49 @@ class MockLogger:
 def message() -> Sequence[str]:
     return ['hello', 'world', 'how', 'are', 'you', 'today?']
 
-def test_program_init_param_msg(message):
+@pytest.mark.parametrize('log_each_line', [True, False])
+def test_program_init_param_msg_no_name(message, log_each_line):
     log = MockLogger()
-    program_init_param_msg(log, message, name=None, log_each_line=False)  # type: ignore
-    assert log.internal
+    program_init_param_msg(log, message, name=None, log_each_line=log_each_line)  # type: ignore
+    assert "\n".join(log.internal).strip() == """
+------
+hello
+world
+how
+are
+you
+today?
+------""".strip()
+
+
+def test_program_init_param_msg_name_no_log_each_line(message):
+    log = MockLogger()
+    program_init_param_msg(log, message, name="my program", log_each_line=False)  # type: ignore
+    assert "\n".join(log.internal).strip() == """
+my program
+------
+hello
+world
+how
+are
+you
+today?
+------""".strip()
+
+
+def test_program_init_param_msg_name_log_each_line(message):
+    log = MockLogger()
+    program_init_param_msg(log, message, name="my program", log_each_line=True)  # type: ignore
+    assert "\n".join(log.internal).strip() == """
+------
+my program
+hello
+world
+how
+are
+you
+today?
+------""".strip()
+
+
+
