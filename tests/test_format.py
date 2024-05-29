7b57777cb4bbd96af3110b193740b6df7c80af9e
Malcolm Greaves <greaves.malcolm@gmail.com>
2024-05-29 12:04:21 +0900

    test exception_stacktrace and test format_stacktrace

--- tests/test_format.py
+++ tests/test_format.py
@@ -3,7 +3,7 @@ from typing import List, Sequence
 
 import pytest
 
-from core_utils.format import program_init_param_msg
+from core_utils.format import exception_stacktrace, format_stacktrace, program_init_param_msg
 
 
 @dataclass
@@ -64,3 +64,28 @@ today?
 
 
 
+def _helper_raise():
+    raise ValueError()
+
+def test_exception_stacktrace():
+    try:
+        _helper_raise()
+    except ValueError as error:
+        stacktrace_lines = exception_stacktrace(error)
+        marker_1 = False
+        marker_2 = False
+        for s in stacktrace_lines:
+            if "in test_exception_stacktrace" in s:
+                marker_1 = True
+            if "in _helper_raise" in s:
+                marker_2 = True
+        assert marker_1
+        assert marker_2
+
+def test_format_stacktrace():
+    try:
+        _helper_raise()
+    except ValueError as error:
+        s = format_stacktrace(error)
+        assert "in test_format_stacktrace" in s
+        assert "in _helper_raise" in s
