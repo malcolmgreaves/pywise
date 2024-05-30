904906df89a62e7618f5b3fecd5d4bf8834ecb7b
Malcolm Greaves <malcolmgreaves@users.noreply.github.com>
2024-05-30 16:44:00 +0900

    [0.7.0] flatten arbitrarily nested iterables (#7)
    New function, `flatten`, in the new module, `core_utils.collections`, provides support for flattening arbitrarily nested iterables. I.e. it can convert a `list[T | list[T]]` into a `list[T]`. Flexible support for either preserving or unrolling tuples (but _never_ `NamedTuple`s) is provided via flag. New supporting function, `is_flattenable`, determines if the input can be flattened or not.
    
    New unit tests provide 100% test coverage of the new `collections` module.

---
+++ core_utils/collections.py
@@ -0,0 +1,71 @@
+from typing import Iterable, Iterator, Sequence, TypeVar, Union
+
+from core_utils.serialization import is_namedtuple
+
+__all__: Sequence[str] = (
+    "Nested",
+    "flatten",
+    "is_flattenable",
+)
+
+T = TypeVar("T")
+
+Nested = Iterable[Union[T, "Nested"]]
+"""Type representing an arbitrary level of nested values.
+"""
+
+
+def flatten(elements: Nested, preserve_tuple: bool = False) -> Nested:
+    """Flattens an arbitrarily-deeply-nested iterable of iterables.
+
+    Uses :func:`is_flattenable` to control what explict types are able to be flattened.
+    Additionally, any `None` values from within the input `elements` will be filtered-out from the
+    final output iterable.
+
+    The flag, `preserve_tuple`, controls whether or not `tuple` instances are flattened.
+    By default, this flag is `False`, meaning that `tuples` are flattned out. I.e. a `List[tuple]`
+    will be transformed into an `Iterable` of all of the individual elements from all `tuple`s in
+    the `list`.
+
+    If this parameter is `True`, then any `tuple` instance encountered is treated as indivisible.
+    However, note that all `NamedTuple` instances are always indivisible: they are treated as
+    encapsulated objects, not as an `Iterable` thing.
+    """
+    if is_flattenable(elements):
+        # The `for` construct iterates over _every_ iterable.
+        for elem in elements:
+            if elem is not None:
+                if preserve_tuple and isinstance(elem, tuple):
+                    yield elem
+
+                elif is_flattenable(elem):
+                    # flatten(elem, ...) will continue to un-roll the nested structure
+                    for sub_elem in flatten(elem, preserve_tuple=preserve_tuple):
+                        yield sub_elem
+                else:
+                    yield elem
+    else:
+        raise ValueError(f"flatten only accepts an iterable, not: {type(elements)}")
+
+
+def is_flattenable(x) -> bool:
+    """Test to see if a value is acceptable for the :func:`flatten` higher-order-function.
+
+    If `True`, then the input is acceptable by :func:`flatten`. `False` otherwise.
+    The following common types are flattenable:
+      - `list`
+      - `Iterator` (includes generators, iterators, map, filter)
+      - tuple (but not a namedtuple - uses :func:`is_namedtuple` to check)
+      - `set`
+      - `range` (e.g. `range(10)` is accepted)`
+
+    Note that the follown common types *ARE NOT* flattenable:
+      - `dict`
+    """
+    return (not isinstance(x, dict)) and (
+        isinstance(x, list)
+        or isinstance(x, Iterator)
+        or (isinstance(x, tuple) and not is_namedtuple(x))
+        or isinstance(x, set)
+        or isinstance(x, range)
+    )
---
+++ tests/test_collections.py
@@ -0,0 +1,123 @@
+from typing import Any, List, NamedTuple, Optional, Tuple, Union
+
+from pytest import raises
+
+from core_utils.collections import flatten, is_flattenable
+
+NT = NamedTuple("NT", [("name", str)])
+
+
+def test_flatten_deep_edge_cases():
+    assert list(flatten([])) == []
+    assert list(flatten(tuple())) == []
+    assert list(flatten(set())) == []
+    assert list(flatten(range(0, 0))) == []
+    assert list(flatten(map(lambda x: x, []))) == []
+    assert list(flatten(filter(lambda x: True, []))) == []
+
+
+def test_flatten_one_level():
+    def simple_generator(limit=5):
+        for i in range(limit):
+            yield i
+
+    def _help_t_flatten_one_level(
+        input_: Optional[Any] = None,
+        output: Optional[Any] = None,
+        to_structure: type = list,
+        is_gen: bool = False,
+    ) -> None:
+        if is_gen:
+            input_ = list(input_)  # type: ignore
+        if output is None:
+            output = input_
+        assert to_structure(flatten(input_)) == output  # type: ignore
+        assert to_structure(flatten(input_, preserve_tuple=True)) == output  # type: ignore
+
+    _help_t_flatten_one_level([1])
+    _help_t_flatten_one_level([10, 20, 30])
+    _help_t_flatten_one_level({10, 20, 30, 40, 50}, to_structure=set)
+    _help_t_flatten_one_level(range(10), output=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    _help_t_flatten_one_level(["hh", "ee", "ll", "ll", "oo"])
+    _help_t_flatten_one_level((1, 2, 3), to_structure=tuple)
+    _help_t_flatten_one_level(range(3), output=(0, 1, 2), to_structure=tuple)
+    _help_t_flatten_one_level(simple_generator(), output=[0, 1, 2, 3, 4], is_gen=True)
+    _help_t_flatten_one_level(map(lambda x: x * 10, range(4)), output=[0, 10, 20, 30], is_gen=True)
+    _help_t_flatten_one_level(
+        filter(lambda x: x % 2 == 0, range(10)), output=[0, 2, 4, 6, 8], is_gen=True
+    )
+
+
+def test_flatten_deep_nested():
+    assert list(flatten([range(10), range(10, 15), range(15, 30)])) == list(range(30))
+
+    multi_level_nested = [
+        [range(10), range(10, 15), range(15, 30)],
+        [range(30, 40)],
+        [
+            [range(40, 50), [[[[range(50, 60)]]]]],
+            [
+                [
+                    [[[[[[[[[[[[[[[[range(60, 70)]]]]]]]]]]]]]]]],
+                    [[range(70, 80)], [[[[range(80, 90)]]]]],
+                    [[range(90, 100), range(100, 100)]],
+                ]
+            ],
+        ],
+    ]
+    assert list(flatten(multi_level_nested)) == list(range(100))
+
+
+def test_flatten_ok_namedtuple_and_tuples():
+    nts = [NT("first"), NT("second"), [NT("third")]]
+    assert list(flatten(nts)) == [NT("first"), NT("second"), NT("third")]
+    assert list(flatten(nts, preserve_tuple=True)) == [NT("first"), NT("second"), NT("third")]
+
+    a_tuple = (1, (2, 3), 4)
+    assert tuple(flatten(a_tuple)) == (1, 2, 3, 4)
+    assert tuple(flatten(a_tuple, preserve_tuple=True)) == (1, (2, 3), 4)
+
+
+def test_flatten_notuple():
+    input_: List[Union[List[Tuple[str, int]], Tuple[str, int]]] = [
+        [("h", 1), ("e", 2), ("l", 3)],
+        [("l", 4)],
+        ("o", 5),
+    ]
+
+    expected: List[Tuple[str, int]] = [("h", 1), ("e", 2), ("l", 3), ("l", 4), ("o", 5)]
+
+    actual_shallow: List[Tuple[str, int]] = list(
+        flatten(
+            input_,  # type: ignore
+            preserve_tuple=True,
+        )
+    )
+    assert actual_shallow == expected
+
+    actual_deep = list(flatten(input_))
+    assert not (actual_deep == expected)
+
+
+def test_is_flattenable():
+    def a_generator():
+        yield 1
+        yield 2
+        yield 3
+
+    assert is_flattenable([1, 2, 3])
+    assert is_flattenable(a_generator())
+    assert is_flattenable((1, 2, 3))
+    assert is_flattenable({1, 2, 3})
+    assert is_flattenable(range(1, 4))
+
+    assert not is_flattenable(None)
+    assert not is_flattenable(NT(name="hello world"))
+    assert not is_flattenable("strings are not flattenable")
+
+
+def test_flatten_fails():
+    d = {"hello": "world"}
+    assert not is_flattenable(d)
+    with raises(ValueError):
+        list(flatten(d))  # type: ignore
